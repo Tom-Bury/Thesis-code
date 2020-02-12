d53af3999560835bfa9858b4b738c105e3ce6a22
Tom Bury <tom.bury@hotmail.com>
2020-02-12 11:45:42 +0100

    Add first components + stub login screen & router outlet

--- Firebase-project/src/app/app-routing.module.ts
+++ Firebase-project/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  pathMatch: 'full'
+}, {
+  path: 'login',
+  component: LoginComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Firebase-project/src/app/app.module.ts
+++ Firebase-project/src/app/app.module.ts
@@ -6,11 +6,25 @@ import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ForumComponent } from './forum/forum.component';
+import { CompareComponent } from './compare/compare.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ForumComponent,
+    CompareComponent,
+    ProfileComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
