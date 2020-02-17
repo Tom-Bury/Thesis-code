b0946b79fee485e53b63101fa05b6c9cfbc4a2be
Tom Bury <tom.bury@hotmail.com>
2020-02-17 21:03:33 +0100

    First working stuff for api

--- Firebase-project/functions/index.js
+++ Firebase-project/functions/index.js
@@ -5,9 +5,8 @@ const express = require('express');
 
 const revproxy = express();
 
-
 revproxy.get('/s/student', (req, res) => {
-  res.send('no');
+  res.send('NO!');
 });
 
 revproxy.get('/s/student/kibana', (req, res) => {
@@ -24,10 +23,16 @@ revproxy.get('/test2', (req, res) => {
 });
 
 revproxy.get('/start', (req, res) => {
-  res.send('Hello.')
+  res.send('Hello!!!!!!')
 });
 
 
+
+
+
+/*
+============================ REVERSE PROXY ============================
+*/
 
 // /s/student/app/kibana#/dashboard/a1a3ac60-19fd-11ea-a467-55964895c522?embed=true&_g=(refreshInterval:(pause:!t,value:0),time:(from:now-31d,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!f),panels:!((embeddableConfig:(title:'Verbruik+in+Watt+doorheen+de+tijd'),gridData:(h:15,i:b6db9ff8-1e24-4335-be89-b6b43d8288e3,w:48,x:0,y:0),id:ce5288e0-167d-11ea-aa54-131c370a98b8,panelIndex:b6db9ff8-1e24-4335-be89-b6b43d8288e3,title:'Verbruik+in+Watt+doorheen+de+tijd',type:visualization,version:'7.4.0')),query:(language:kuery,query:''),timeRestore:!f,title:'Gewoon+een+watt+timeline',viewMode:view)
 
@@ -46,8 +51,36 @@ revproxy.use(proxy('https://932f51d533944da9b8ecace37e13aa8b.europe-west1.gcp.cl
 }));
 
 
+
+/*
+============================ API ============================
+*/
+const thesisApi = express();
+
+thesisApi.get('/thesisApi', (req, res) => {
+  res.send('<h1>Hello world from the api!</h1>');
+})
+
+thesisApi.get('/thesisApi/', (req, res) => {
+  res.send('<h1>Hello world from the api!</h1>');
+})
+
+
+thesisApi.get('/thesisApi/ok', (req, res) => {
+  res.send('<h1>Hello world from the api!</h1>');
+})
+
+thesisApi.get('/ok', (req, res) => {
+  res.send('<h1>Hello world from the api!</h1>');
+})
+
+
+
+exports.thesisApi = functions.https.onRequest(thesisApi);
+
 // Create "main" function to host all other top-level functions
 const revrproxyMain = express();
 revrproxyMain.use('', revproxy);
+
 
 exports.revproxyMain = functions.https.onRequest(revrproxyMain);
