f15e743a7ea29b53a7df507c4fc4a5aecf6c33a8
Tom Bury <tom.bury@hotmail.com>
2020-02-20 11:22:09 +0100

    Basic stub summary component

--- Firebase-project/src/app/home/summary/summary.component.ts
+++ Firebase-project/src/app/home/summary/summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SummaryTableEntry } from 'src/app/shared/interfaces/summary-table-entry.model';
 
 @Component({
   selector: 'app-summary',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummaryComponent implements OnInit {
 
+  currWeekAverage = 32.15;
+
+  summaryEntries: SummaryTableEntry[] = [
+    new SummaryTableEntry('Today\'s total usage so far', 12.36, 'kWh', false),
+    new SummaryTableEntry('This week\'s average usage', 32.15, 'kWh', false),
+    new SummaryTableEntry('Yesterday\'s total usage', 38.47, 'kWh', true),
+    new SummaryTableEntry('Best day this week', 27.99, 'kWh', true),
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  calculatePercentageString(entry: SummaryTableEntry): string {
+    const percentage = this.calculatePercentage(entry);
+    const prefix = percentage > 0 ? '+' : '';
+    return prefix + percentage.toFixed(2);
+  }
+
+  calculatePercentage(entry: SummaryTableEntry): number {
+    const result = 100 * (entry.value / this.currWeekAverage) - 100;
+    return result;
+  }
 }
---
+++ Firebase-project/src/app/shared/interfaces/summary-table-entry.model.ts
@@ -0,0 +1,10 @@
+export class SummaryTableEntry {
+
+  constructor(
+    public name: string,
+    public value: number,
+    public metric: string,
+    public useExtraValue: boolean,
+  ) {}
+
+}
