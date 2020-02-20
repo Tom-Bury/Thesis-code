62fa5e5f28c52aa0c36fd94f99a2f1de55de0c33
Tom Bury <tom.bury@hotmail.com>
2020-02-20 15:56:51 +0100

    Fix leaderboard column width

--- Firebase-project/src/app/forum/leaderboard/leaderboard.component.ts
+++ Firebase-project/src/app/forum/leaderboard/leaderboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/interfaces/user.model';
 
 @Component({
   selector: 'app-leaderboard',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaderboardComponent implements OnInit {
 
+  public users: User[] = [
+    new User('Benjamin', 1023),
+    new User('Dries', 824),
+    new User('Eva', 965),
+    new User('Sarah', 124),
+    new User('Hannah', 563),
+    new User('Liesbeth', 741),
+    new User('Laurens', 1112),
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.users = this.users.sort(User.compareUsersByScore);
   }
+
 
 }
---
+++ Firebase-project/src/app/shared/interfaces/user.model.ts
@@ -0,0 +1,18 @@
+export class User {
+
+  constructor(
+    public name,
+    public score
+  ) {}
+
+  public static compareUsersByScore(a: User, b: User) {
+    if ( a.score < b.score ) {
+      return -1;
+    }
+    if ( a.score > b.score ) {
+      return 1;
+    }
+    return 0;
+  }
+
+}
