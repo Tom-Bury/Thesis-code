8edbbfbb3fd10e0983fa882fd472f4ae2a170642
Tom Bury <tom.bury@hotmail.com>
2020-02-21 12:37:49 +0100

    First version of social count component

--- Firebase-project/src/app/forum-post/forum-post.component.ts
+++ Firebase-project/src/app/forum-post/forum-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ForumComment } from '../shared/interfaces/forum-comment.model';
 import {Location} from '@angular/common';
+import { Like } from '../shared/interfaces/like.model';
 
 @Component({
   selector: 'app-forum-post',
@@ -10,14 +11,14 @@ import {Location} from '@angular/common';
 export class ForumPostComponent implements OnInit {
 
   dummyThread = [
-      new ForumComment('User 1', 'This is my comment!', 'some date', 2, [], '1'),
-      new ForumComment('User 2', 'This is another comment, with subcomments!', 'other date', 10, [
-        new ForumComment('User 1', 'Cool!', 'some date', 0, [], '3'),
-        new ForumComment('User 3', 'Yeah!', 'date', 3, [
-          new ForumComment('User 2', 'Indeed :)', 'now', 0, [], '6')
+      new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1'),
+      new ForumComment('User 2', 'This is another comment, with subcomments!', 'other date', [new Like(null, null), new Like(null, null), new Like(null, null), new Like(null, null)], [
+        new ForumComment('User 1', 'Cool!', 'some date', [new Like(null, null), new Like(null, null), new Like(null, null), new Like(null, null), new Like(null, null), new Like(null, null)], [], '3'),
+        new ForumComment('User 3', 'Yeah!', 'date', [], [
+          new ForumComment('User 2', 'Indeed :)', 'now', [new Like(null, null), new Like(null, null)], [], '6')
         ], '2')
       ], '4'),
-      new ForumComment('user 3', 'Wow...', 'date', 3, [], '5')
+      new ForumComment('user 3', 'Wow...', 'date', [new Like(null, null)], [], '5')
   ];
 
   constructor(
--- Firebase-project/src/app/forum/your-posts-card/your-posts-card.component.ts
+++ Firebase-project/src/app/forum/your-posts-card/your-posts-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ForumPost } from 'src/app/shared/interfaces/forum-post.model';
+import { User } from 'src/app/shared/interfaces/user.model';
+import { Like } from 'src/app/shared/interfaces/like.model';
+import { ForumComment } from 'src/app/shared/interfaces/forum-comment.model';
 
 @Component({
   selector: 'app-your-posts-card',
@@ -6,6 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./your-posts-card.component.scss']
 })
 export class YourPostsCardComponent implements OnInit {
+
+  yourPosts: ForumPost[] = [
+    new ForumPost('This is my first post', 'Some content', new User('Tom', 100), [new Like(null, null), new Like(null, null)], [new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1'), new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1')]),
+    new ForumPost('This is my second post', 'Some content', new User('Tom', 100), [new Like(null, null), new Like(null, null)], [new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1'), new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1')]),
+    new ForumPost('This is my third post', 'Some content', new User('Tom', 100), [new Like(null, null), new Like(null, null)], [new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1'), new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1')]),
+    new ForumPost('This is my fourth post', 'Some content', new User('Tom', 100), [new Like(null, null), new Like(null, null)], [new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1'), new ForumComment('User 1', 'This is my comment!', 'some date', [new Like(null, null), new Like(null, null)], [], '1')]),
+  ];
 
   constructor() { }
 
