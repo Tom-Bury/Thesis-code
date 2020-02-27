033515c0dd8ea446332dcf6391556c7a30d84a9b
Tom Bury <tom.bury@hotmail.com>
2020-02-27 21:31:09 +0100

    Refactor api.js: add comments & file structure

--- Firebase-project/functions/api.js
+++ Firebase-project/functions/api.js
@@ -18,16 +18,25 @@ const client = new Client({
  * API =================================================================================================================
  */
 
+ // ==
+ // == /
+ // ==
 api.get('/', (req, res) => {
   AU.sendResponse(res, false, 'Hello from the API!');
 })
 
+// ==
+// == /overview
+// ==
 api.get('/overview', (req, res) => {
   res.json({
     totalKwh: 'Usage: /api/totalkwh?from=DD/MM/YYYY-HH:mm&to=DD/MM/YYYY-HH:mm. The to parameter is optional, if not specified current date is used. Times are also optional, if not specified 00:00 will be used.'
   })
 })
 
+// ==
+// == /totalKwh
+// ==
 api.get('/totalKwh', async (req, res) => {
   try {
     const timeframe = AU.getTimeframeFromRequest(req, res);
@@ -47,28 +56,9 @@ api.get('/totalKwh', async (req, res) => {
   }
 })
 
-
-
-
-async function getFuseWhAt(res, atTime, queryFuseDescription) {
-  try {
-    const timeFrom = AU.toElasticDatetimeString(atTime.subtract(1, 'm'));
-    const timeTo = AU.toElasticDatetimeString(atTime.add(1, 'm'));
-
-    let query = QUERIES.MAX_WH_FOR_FUSE_QUERY;
-    query.body.query.bool.filter[0].match_phrase.fuseDescription = queryFuseDescription.replace(/_/g, " ");
-    query.body.query.bool.filter[1].range["@timestamp"].gte = timeFrom;
-    query.body.query.bool.filter[1].range["@timestamp"].lte = timeTo;
-
-    const result = await client.search(query);
-    return result.body.aggregations.maxFuseWh.value;
-
-  } catch (err) {
-    throw err;
-  }
-}
-
-
+// ==
+// == /fuseKwhBetween
+// ==
 api.get('/fuseKwhBetween', async (req, res) => {
   try {
     const timeFrom = AU.getDateTimeFromRequest(req, 'from');
@@ -89,8 +79,31 @@ api.get('/fuseKwhBetween', async (req, res) => {
   } catch (err) {
     AU.sendResponse(res, true, err);
   }
-
 })
+
+
+
+/**
+ * HELPER FUNCTIONS =================================================================================================================
+ */
+
+async function getFuseWhAt(res, atTime, queryFuseDescription) {
+  try {
+    const timeFrom = AU.toElasticDatetimeString(atTime.subtract(1, 'm'));
+    const timeTo = AU.toElasticDatetimeString(atTime.add(1, 'm'));
+
+    let query = QUERIES.MAX_WH_FOR_FUSE_QUERY;
+    query.body.query.bool.filter[0].match_phrase.fuseDescription = queryFuseDescription.replace(/_/g, " ");
+    query.body.query.bool.filter[1].range["@timestamp"].gte = timeFrom;
+    query.body.query.bool.filter[1].range["@timestamp"].lte = timeTo;
+
+    const result = await client.search(query);
+    return result.body.aggregations.maxFuseWh.value;
+
+  } catch (err) {
+    throw err;
+  }
+}
 
 
 module.exports = api;
