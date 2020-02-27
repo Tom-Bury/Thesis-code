efbe2a88832225bcdc77d600811507c43c499c91
Tom Bury <tom.bury@hotmail.com>
2020-02-27 14:29:49 +0100

    Add verbruiksverloop sample kibana iframe

--- Firebase-project/src/app/compare/verbruiksverloop/verbruiksverloop.component.ts
+++ Firebase-project/src/app/compare/verbruiksverloop/verbruiksverloop.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-verbruiksverloop',
@@ -7,9 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerbruiksverloopComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {}
 
 }
