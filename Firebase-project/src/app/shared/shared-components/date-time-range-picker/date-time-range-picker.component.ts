56333a0bbcc4bd3a24c020fb262831685676c5b1
Tom Bury <tom.bury@hotmail.com>
2020-03-03 09:12:05 +0100

    Submitted range shows up in selector of datetime range component

--- Firebase-project/src/app/shared/shared-components/date-time-range-picker/date-time-range-picker.component.ts
+++ Firebase-project/src/app/shared/shared-components/date-time-range-picker/date-time-range-picker.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbDateStruct, NgbDate } from '@ng-bootstrap/ng-bootstrap';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  NgbDateStruct,
+  NgbDate,
+  NgbTimeStruct
+} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-date-time-range-picker',
   templateUrl: './date-time-range-picker.component.html',
@@ -7,30 +14,57 @@ import { NgbDateStruct, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 })
 export class DateTimeRangePickerComponent implements OnInit {
 
-  pickedRange: string;
-
-
-  hoveredDate: NgbDate;
-
+  pickedRange: string[] = ['from', 'to'];
   fromDate: NgbDate;
   toDate: NgbDate;
 
-  constructor() { }
+  hoveredDate: NgbDate;
 
-  ngOnInit(): void {
+  private dummyTime: NgbTimeStruct = {
+    hour: 0,
+    minute: 0,
+    second: 0
   }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
 
   clear(): void {
     this.fromDate = null;
     this.toDate = null;
-    this.pickedRange = null;
+    this.pickedRange = ['from', 'to'];
   }
 
   submit(): void {
-    this.pickedRange = 'This was picked';
+    if (this.fromDate) {
+      this.pickedRange[0] = this.dateToString(this.fromDate, this.dummyTime);
+
+      if (this.toDate) {
+        this.pickedRange[1] = this.dateToString(this.toDate, this.dummyTime);
+      } else {
+        this.pickedRange[1] = this.dateToString(this.fromDate, this.dummyTime);
+      }
+    } else {
+      this.pickedRange = ['?', '?'];
+    }
   }
 
 
+  dateToString(date: NgbDate, time: NgbTimeStruct): string {
+    const dateStr = date.day + '/' + date.month + '/' + date.year;
+    const timeStr = time.hour + ':' + time.minute;
+    return dateStr + ' @' + timeStr;
+  }
+
+
+
+
+
+  /**
+   * Methods for adding / removing classes on the calendar element
+   */
 
   onDateSelection(date: NgbDate) {
     if (!this.fromDate && !this.toDate) {
@@ -48,7 +82,6 @@ export class DateTimeRangePickerComponent implements OnInit {
     return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
   }
 
-
   // From date & to date are selected. Used to color dates in between.
   isInRangeExclusive(date: NgbDate) {
     return date.after(this.fromDate) && date.before(this.toDate);
@@ -57,5 +90,6 @@ export class DateTimeRangePickerComponent implements OnInit {
   isInRangeInclusive(date: NgbDate) {
     return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInRangeExclusive(date);
   }
+
 
 }
