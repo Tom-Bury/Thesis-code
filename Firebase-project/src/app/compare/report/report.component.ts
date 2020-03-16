77c35ada2d66676ed594d830d95bfcd30a874e43
Tom Bury <tom.bury@hotmail.com>
2020-03-16 10:13:19 +0100

    Remove unnecessary imports from report.ts

--- Firebase-project/src/app/compare/report/report.component.ts
+++ Firebase-project/src/app/compare/report/report.component.ts
@@ -1,47 +1,16 @@
 import {
   Component,
   OnInit,
-  ViewChild,
-  ElementRef,
-  AfterViewInit,
 } from '@angular/core';
 
-import {
-  DataFetcherService
-} from 'src/app/shared/services/data-fetcher.service';
-import {
-  DateTimeRangePickerComponent
-} from 'src/app/shared/shared-components/date-time-range-picker/date-time-range-picker.component';
-import {
-  DatetimeRange
-} from 'src/app/shared/interfaces/datetime-range.model';
 import * as moment from 'moment';
 import {
   toNgbDate
 } from 'src/app/shared/global-functions';
 import {
-  ApiStatistics
-} from 'src/app/shared/interfaces/api-interfaces/api-statistics.model';
-import {
   NgbDate
 } from '@ng-bootstrap/ng-bootstrap';
 
-import {
-  ApexAxisChartSeries,
-  ApexChart,
-  ChartComponent,
-  ApexDataLabels,
-  ApexPlotOptions,
-  ApexYAxis,
-  ApexTitleSubtitle,
-  ApexXAxis,
-  ApexFill,
-  ApexStroke,
-  ApexLegend,
-  ApexTooltip,
-  ApexGrid,
-  ApexNoData
-} from 'ng-apexcharts';
 
 
 
@@ -60,9 +29,7 @@ export class ReportComponent implements OnInit {
   [moment().day(1), moment().day(7)].map(toNgbDate);
 
 
-  constructor(
-    private dataFetcherSvc: DataFetcherService
-  ) {
+  constructor() {
     this.initDateRanges = [this.currWeek];
    }
 
