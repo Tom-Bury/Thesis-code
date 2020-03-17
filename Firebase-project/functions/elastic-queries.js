52fd15e89afa6785fc6297f03a82ce56b2026e84
Tom Bury <tom.bury@hotmail.com>
2020-03-17 09:40:32 +0100

    Add fuses.json & sensorIds.json

--- Firebase-project/functions/elastic-queries.js
+++ Firebase-project/functions/elastic-queries.js
@@ -1,4 +1,106 @@
 module.exports = {
+
+  FUSES: {
+    "SC serverlokaal": {
+      "sensorId": ["0c5870ce-0e8d-44ca-801c-10a00ff65ba9"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Voeding vaatwas": {
+      "sensorId": ["3b927c9c-7186-4556-98fc-af66fd765689"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Voeding ventilatie": {
+      "sensorId": ["421a7cf1-11a9-43f3-84ea-b188433e852c"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Licht & SC WC & stookplaats": {
+      "sensorId": ["9b16c7a8-b38a-4b07-b114-aefee392f8eb"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Voeding boiler": {
+      "sensorId": ["8eb29d17-7b00-4f72-b279-7d4b2bb65973"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Voedingen Velux achter bureau 2, uitbereiding alarm": {
+      "sensorId": ["77d38094-c1e0-4122-98dc-1b8cd8fc1eb8"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Licht bureau 1, inkomzone, traphal, vergaderlokaal 2": {
+      "sensorId": ["1b4c1086-9d2e-4e9e-8235-3747bfc1200b"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Licht directielokaal, vergaderlokaal 1, terras, bureau 2": {
+      "sensorId": ["f812c815-10b2-4e64-be7d-09d6bcea0ff3"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "SC & licht keuken": {
+      "sensorId": ["9764a3ad-2ee1-4382-bb0e-db64daa9a1b2"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Vloerdoos vergaderlokaal 1": {
+      "sensorId": ["ae1a9bd4-f16d-4934-a67c-ec9ad4d3bd3e"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Vloerdoos bureau 1": {
+      "sensorId": ["6c4da608-8e49-4917-90db-ff92a889b364", "44494f41-f865-4cad-9032-e968148b5865", "f390d66f-4901-4dce-bf24-51d862f0feea"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Vloerdoos bureau 2": {
+      "sensorId": ["80b0f3d9-8dac-4253-93cf-a06e4f75b5b7", "8a472d54-865c-4dcd-8e96-7170ebb86d41", "07a6cd16-a225-49b1-b064-b340930c9ea4", "16075fc4-ff32-4075-999b-2bb39dd0da71"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "SC inkomzone, vergaderlokaal 1 & 2, directielokaal, velux achter": {
+      "sensorId": ["4cd4ac9e-06d9-4c9c-9e6d-fb7e7b958b7d"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "SC bureau 1, licht serverlokaal, licht & SC berging & bureau's boven": {
+      "sensorId": ["758b6544-5cd6-4dda-97f3-a8aaadbb770a"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    },
+    "Licht centraal LED-strip": {
+      "sensorId": ["b6500101-ef5b-42f0-a332-2a61d25231cb"],
+      "concentratorId": ["23915b22-462c-401b-8d66-800d7ebcc667"],
+      "gatewayId": ["b1ef86b6-099d-4a18-88bf-ccb60f88f25a"]
+    }
+  },
+
+  SENSOR_IDS: {
+    "0c5870ce-0e8d-44ca-801c-10a00ff65ba9": "SC serverlokaal",
+    "3b927c9c-7186-4556-98fc-af66fd765689": "Voeding vaatwas",
+    "421a7cf1-11a9-43f3-84ea-b188433e852c": "Voeding ventilatie",
+    "9b16c7a8-b38a-4b07-b114-aefee392f8eb": "Licht & SC WC & stookplaats",
+    "8eb29d17-7b00-4f72-b279-7d4b2bb65973": "Voeding boiler",
+    "77d38094-c1e0-4122-98dc-1b8cd8fc1eb8": "Voedingen Velux achter bureau 2, uitbereiding alarm",
+    "1b4c1086-9d2e-4e9e-8235-3747bfc1200b": "Licht bureau 1, inkomzone, traphal, vergaderlokaal 2",
+    "f812c815-10b2-4e64-be7d-09d6bcea0ff3": "Licht directielokaal, vergaderlokaal 1, terras, bureau 2",
+    "9764a3ad-2ee1-4382-bb0e-db64daa9a1b2": "SC & licht keuken",
+    "ae1a9bd4-f16d-4934-a67c-ec9ad4d3bd3e": "Vloerdoos vergaderlokaal 1",
+    "6c4da608-8e49-4917-90db-ff92a889b364": "Vloerdoos bureau 1",
+    "44494f41-f865-4cad-9032-e968148b5865": "Vloerdoos bureau 1",
+    "f390d66f-4901-4dce-bf24-51d862f0feea": "Vloerdoos bureau 1",
+    "80b0f3d9-8dac-4253-93cf-a06e4f75b5b7": "Vloerdoos bureau 2",
+    "8a472d54-865c-4dcd-8e96-7170ebb86d41": "Vloerdoos bureau 2",
+    "07a6cd16-a225-49b1-b064-b340930c9ea4": "Vloerdoos bureau 2",
+    "16075fc4-ff32-4075-999b-2bb39dd0da71": "Vloerdoos bureau 2",
+    "4cd4ac9e-06d9-4c9c-9e6d-fb7e7b958b7d": "SC inkomzone, vergaderlokaal 1 & 2, directielokaal, velux achter",
+    "758b6544-5cd6-4dda-97f3-a8aaadbb770a": "SC bureau 1, licht serverlokaal, licht & SC berging & bureau's boven",
+    "b6500101-ef5b-42f0-a332-2a61d25231cb": "Licht centraal LED-strip"
+  },
+
   TOTAL_WH_QUERY: {
     index: "*",
     body: {
---
+++ Varia/extractor.py
@@ -0,0 +1,57 @@
+# Input file: Time  fuseDescription \n sensorId \n concentratorId \n gatewayId
+import json
+
+
+# Fuses.json
+
+currLineType = 0
+fuses = {"fuseName": {"sensorId": 1, "concentratorId": 1, "gatewayId": 1}}
+with open("SampleData.txt") as fp: 
+    fuse = ""
+    for line in fp:
+    
+        if line[-1:] == "\n":
+            line = line[:len(line)-1]
+        
+        if currLineType == 0:
+            fuse = line[26:]
+            if not fuse in fuses:
+                fuses[fuse] = {"sensorId": [], "concentratorId": [], "gatewayId": []}
+        elif currLineType == 1:
+            if line not in fuses[fuse]["sensorId"]:
+                fuses[fuse]["sensorId"].append(line)
+        elif currLineType == 2:
+            if line not in fuses[fuse]["concentratorId"]:
+                fuses[fuse]["concentratorId"].append(line)
+        else:
+            if line not in fuses[fuse]["gatewayId"]:
+                fuses[fuse]["gatewayId"].append(line)
+             
+        currLineType = (currLineType + 1) % 4
+        
+with open('fuses.json', 'w') as fp:
+    json.dump(fuses, fp)
+    
+    
+    
+# SensorID -> FuseDescription
+
+currLineType = 0
+sensorIds = {}
+with open("SampleData.txt") as fp: 
+    fuse = ""
+    for line in fp:
+    
+        if line[-1:] == "\n":
+            line = line[:len(line)-1]
+        
+        if currLineType == 0:
+            fuse = line[26:]
+        elif currLineType == 1:
+            if line not in sensorIds:
+                sensorIds[line] = fuse
+        
+        currLineType = (currLineType + 1) % 4
+with open('sensorIds.json', 'w') as fp:
+    json.dump(sensorIds, fp)
+    
