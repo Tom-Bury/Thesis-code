24f2374601df3b3e67390227ecb450d3ec340265
Tom Bury <tom.bury@hotmail.com>
2020-03-20 16:38:57 +0100

    Add data is unavailable to fuse metrics

--- Firebase-project/src/app/compare/statistics/per-fuse-stats/per-fuse-stats.component.ts
+++ Firebase-project/src/app/compare/statistics/per-fuse-stats/per-fuse-stats.component.ts
@@ -32,6 +32,7 @@ export class PerFuseStatsComponent implements OnInit {
   } [] = [];
 
   public chartOptions: Partial < ChartOptions > ;
+  public dataIsUnavailable = false;
 
 
   constructor(
@@ -46,6 +47,9 @@ export class PerFuseStatsComponent implements OnInit {
       labels: [],
       noData: {
         text: 'Data is unavailable'
+      },
+      legend: {
+        position: 'bottom'
       }
     };
   }
@@ -53,6 +57,7 @@ export class PerFuseStatsComponent implements OnInit {
   ngOnInit(): void {}
 
   fetchNewData(newRange: DatetimeRange): void {
+    this.dataIsUnavailable = false;
     this.dataFetcherSvc.getFusesKwh(newRange.fromDate, newRange.fromTime, newRange.toDate, newRange.toTime).subscribe(
       (data) => {
         if (!data.isError) {
@@ -64,15 +69,16 @@ export class PerFuseStatsComponent implements OnInit {
           });
 
           this.data.sort((a, b) => b.kwh - a.kwh);
-
         } else {
           console.error('Something wrong with the received data', data);
           this.data = [];
+          this.dataIsUnavailable = true;
         }
       },
       (error) => {
         console.error(error);
         this.data = [];
+        this.dataIsUnavailable = true;
       },
       () => {
         this.updateChart();
@@ -82,22 +88,28 @@ export class PerFuseStatsComponent implements OnInit {
 
   private updateChart(percentage = 0.8): void {
 
-    const topPercentage = this.data.reduce((a, b) => a + b.kwh, 0) * percentage;
-    const pieChartData = [];
-    let currTotalKwh = 0;
-    let restKwh = 0;
-    this.data.forEach(d => {
-      currTotalKwh += d.kwh;
-      if (currTotalKwh <= topPercentage) {
-        pieChartData.push(d);
-      } else {
-        restKwh += d.kwh;
-      }
-    });
-    pieChartData.push({fuse: 'Others', kwh: Math.round((restKwh + Number.EPSILON) * 100) / 100});
+    if (this.data.length > 0) {
+      const topPercentage = this.data.reduce((a, b) => a + b.kwh, 0) * percentage;
+      const pieChartData = [];
+      let currTotalKwh = 0;
+      let restKwh = 0;
+      this.data.forEach(d => {
+        currTotalKwh += d.kwh;
+        if (currTotalKwh <= topPercentage) {
+          pieChartData.push(d);
+        } else {
+          restKwh += d.kwh;
+        }
+      });
+      pieChartData.push({fuse: 'Others', kwh: Math.round((restKwh + Number.EPSILON) * 100) / 100});
 
-    this.chartOptions.series = pieChartData.map(d => d.kwh);
-    this.chartOptions.labels = pieChartData.map(d => d.fuse);
+      this.chartOptions.series = pieChartData.map(d => d.kwh);
+      this.chartOptions.labels = pieChartData.map(d => d.fuse);
+    } else {
+      this.chartOptions.series = [];
+      this.chartOptions.labels = [];
+    }
+
   }
 
 
