b4baec841d69475fada8577542cde0c678ed9243
Tom Bury <tom.bury@hotmail.com>
2020-03-25 12:22:53 +0100

    Show liked or not in header of post-card and forumpost

--- Firebase-project/src/app/forum/forum-post-card/forum-post-card.component.ts
+++ Firebase-project/src/app/forum/forum-post-card/forum-post-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ForumPost } from 'src/app/shared/interfaces/forum/forum-post.model';
 import { AllUsersService } from 'src/app/shared/services/all-users.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-forum-post-card',
@@ -16,7 +17,8 @@ export class ForumPostCardComponent implements OnInit {
   constructor(
     private allUsersSvc: AllUsersService,
     private router: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private currUser: UserService
   ) {
   }
 
@@ -26,6 +28,10 @@ export class ForumPostCardComponent implements OnInit {
 
   public routeToPost(): void {
     this.router.navigate(['post', this.post.getID()], {relativeTo: this.activatedRoute});
+  }
+
+  public hasLiked(): boolean {
+    return this.currUser.userHasLikedPost(this.post.getID()) !== 'false';
   }
 
 }
