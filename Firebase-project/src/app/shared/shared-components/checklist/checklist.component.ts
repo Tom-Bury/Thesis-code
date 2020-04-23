e1fef680d3305f0dcbd203bc4b4d1890b1cfb13b
Tom Bury <tom.bury@hotmail.com>
2020-04-23 16:00:54 +0200

    Add extra checklist items

--- Firebase-project/src/app/shared/shared-components/checklist/checklist.component.ts
+++ Firebase-project/src/app/shared/shared-components/checklist/checklist.component.ts
@@ -9,12 +9,14 @@ import { ChecklistItem } from '../../interfaces/checklist-item.model';
 export class ChecklistComponent implements OnInit {
 
   checklistItems: ChecklistItem[] = [
-    new ChecklistItem('Came by bike', 25, true),
+    new ChecklistItem('Came by bike', 50, true),
     new ChecklistItem('Took stairs instead of elevator', 20, false),
     new ChecklistItem('Turned of monitor during breaks', 15, false),
     new ChecklistItem('Put on a sweater for the cold', 10, true),
     new ChecklistItem('Unplugged unused phone charger', 10, false),
     new ChecklistItem('Only cooked the amount of water I needed', 10, true),
+    new ChecklistItem('Carpooled', 20, false),
+    new ChecklistItem('Turned off unnecessary lights', 15, false)
   ];
 
   constructor() { }
