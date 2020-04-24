b0f99ed1a2c8ca670e3af05fc60bedf07c0a62bb
Tom Bury <tom.bury@hotmail.com>
2020-04-24 20:08:04 +0200

    Make circuit table into bar chart instead in summary report

--- Firebase-project/src/app/compare/statistics/per-fuse-stats/per-fuse-stats.component.ts
+++ Firebase-project/src/app/compare/statistics/per-fuse-stats/per-fuse-stats.component.ts
@@ -24,6 +24,8 @@ import {
 })
 export class PerFuseStatsComponent implements OnInit {
 
+   @ViewChild('barChart') barChart: ChartComponent;
+
   @Input() id = 1;
   public isOpened = false;
   public data: {
@@ -31,14 +33,13 @@ export class PerFuseStatsComponent implements OnInit {
     kwh: number
   } [] = [];
 
-  public chartOptions: Partial < ChartOptions > ;
-  public dataIsUnavailable = false;
-
+  public pieChartOptions: Partial < ChartOptions > ;
+  public barChartOptions: Partial < ChartOptions > ;
 
   constructor(
     private dataFetcherSvc: DataFetcherService
   ) {
-    this.chartOptions = {
+    this.pieChartOptions = {
       series: [],
       chart: {
         height: 350,
@@ -50,6 +51,165 @@ export class PerFuseStatsComponent implements OnInit {
       },
       legend: {
         position: 'bottom'
+      },
+      tooltip: {
+        enabled: true,
+        followCursor: true,
+        fillSeriesColor: false,
+        theme: 'light',
+        style: {
+          fontSize: '12px',
+          fontFamily: 'inherit'
+        },
+        onDatasetHover: {
+          highlightDataSeries: true,
+        },
+        x: {
+          show: true,
+          formatter: (val, { series, seriesIndex, dataPointIndex, w }) => {
+            return '<b>' + val + '</b>';
+          },
+        },
+        y: {
+          title: {
+            formatter: (seriesName) => seriesName
+          },
+          formatter: (value, {
+            series,
+            seriesIndex,
+            dataPointIndex,
+            w
+          }) => {
+            return '<b>' + value + ' kWhs</b>';
+          }
+        },
+        marker: {
+          show: true,
+        },
+      }
+    };
+
+    this.barChartOptions = {
+      series: [{
+        name: 'Total usage per circuit',
+        data: []
+      }],
+      chart: {
+        height: 500,
+        type: 'bar',
+        zoom: {
+          enabled: false
+        },
+        toolbar: {
+          show: false
+        }
+      },
+      grid: {
+        row: {
+          colors: ['#fff', '#f8f9fa']
+        }
+      },
+      legend: {
+        show: false
+      },
+      noData: {
+        text: 'Data is unavailable.'
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+          dataLabels: {
+            position: 'top'
+          }
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: [0],
+        formatter: (val) => {
+          return val.toFixed(1) + ' kWh';
+        },
+        style: {
+          fontSize: '12px',
+          colors: ['#212529']
+        },
+        textAnchor: 'start',
+        offsetX: 5
+      },
+      xaxis: {
+        categories: [],
+        title: {
+          text: '',
+          style: {
+            fontWeight: 600
+          }
+        },
+        tickPlacement: 'between',
+        tooltip: {
+          enabled: false
+        }
+      },
+      yaxis: {
+        axisBorder: {
+          show: true
+        },
+        title: {
+          text: 'kWhs',
+          style: {
+            fontWeight: 600
+          }
+        },
+        axisTicks: {
+          show: true
+        },
+        labels: {
+          formatter: (val) => {
+            return val.toString();
+          }
+        }
+      },
+      title: {
+        text: 'Total usage per circuit',
+        align: 'left',
+        style: {
+          fontWeight: 600,
+          fontFamily: 'inherit'
+        }
+      },
+      tooltip: {
+        enabled: true,
+        followCursor: true,
+        fillSeriesColor: false,
+        theme: 'light',
+        style: {
+          fontSize: '12px',
+          fontFamily: 'inherit'
+        },
+        onDatasetHover: {
+          highlightDataSeries: true,
+        },
+        x: {
+          show: true,
+          formatter: (val, { series, seriesIndex, dataPointIndex, w }) => {
+            return '<b>Total usage per circuit</b>';
+          },
+        },
+        y: {
+          title: {
+            formatter: (seriesName) => ''
+          },
+          formatter: (value, {
+            series,
+            seriesIndex,
+            dataPointIndex,
+            w
+          }) => {
+            return this.data.map(d => d.fuse)[seriesIndex].replace(/ SC/g, ' stopcontacten').replace(/SC/g, 'Stopcontacten') + ': <b>' + value + ' kWh</b>';
+          }
+        },
+        marker: {
+          show: true,
+        },
       }
     };
   }
@@ -57,7 +217,6 @@ export class PerFuseStatsComponent implements OnInit {
   ngOnInit(): void {}
 
   fetchNewData(newRange: DatetimeRange): void {
-    this.dataIsUnavailable = false;
     this.dataFetcherSvc.getFusesKwh(newRange.fromDate, newRange.fromTime, newRange.toDate, newRange.toTime).subscribe(
       (data) => {
         if (!data.isError) {
@@ -74,21 +233,20 @@ export class PerFuseStatsComponent implements OnInit {
         } else {
           console.error('Something wrong with the received data', data);
           this.data = [];
-          this.dataIsUnavailable = true;
         }
       },
       (error) => {
         console.error(error);
         this.data = [];
-        this.dataIsUnavailable = true;
       },
       () => {
-        this.updateChart();
+        this.updatePieChart();
+        this.updateBarChart();
       }
     );
   }
 
-  private updateChart(percentage = 0.8): void {
+  private updatePieChart(percentage = 0.8): void {
 
     if (this.data.length > 0) {
       const topPercentage = this.data.reduce((a, b) => a + b.kwh, 0) * percentage;
@@ -105,14 +263,25 @@ export class PerFuseStatsComponent implements OnInit {
       });
       pieChartData.push({fuse: 'Others', kwh: Math.round((restKwh + Number.EPSILON) * 100) / 100});
 
-      this.chartOptions.series = pieChartData.map(d => d.kwh);
-      this.chartOptions.labels = pieChartData.map(d => d.fuse);
+      this.pieChartOptions.series = pieChartData.map(d => d.kwh);
+      this.pieChartOptions.labels = pieChartData.map(d => d.fuse);
     } else {
-      this.chartOptions.series = [];
-      this.chartOptions.labels = [];
+      this.pieChartOptions.series = [];
+      this.pieChartOptions.labels = [];
     }
 
   }
 
+
+  private updateBarChart(): void {
+    const newBarChartData = {
+      name: 'Total usage per circuit',
+      data: this.data.map(d => d.kwh)
+    };
+    this.barChartOptions.xaxis.categories = this.data.map(d => d.fuse);
+    this.barChartOptions.series[0] = newBarChartData;
+
+    this.barChart.updateOptions(this.barChartOptions);
+  }
 
 }
