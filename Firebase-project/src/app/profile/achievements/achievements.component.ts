16310448caa01a925c9d60407865f31ec49c7792
Tom Bury <tom.bury@hotmail.com>
2020-05-01 14:45:34 +0200

    Add explanation to achievements

--- Firebase-project/src/app/profile/achievements/achievements.component.ts
+++ Firebase-project/src/app/profile/achievements/achievements.component.ts
@@ -12,8 +12,12 @@ export class AchievementsComponent implements OnInit {
     new Achievement('First login ever!', 10, true),
     new Achievement('Logged in each day of a work week', 15, true),
     new Achievement('Logged in each day for 2 weeks straight', 30, false),
-    new Achievement('First post put on forum', 20, true),
-    new Achievement('Gained 10 likes on a forum post', 20, false),
+    new Achievement('First post put on the discussion board', 20, true),
+    new Achievement('Gained a comment on a post', 10, true),
+    new Achievement('Gained 5 comments on a post', 40, false),
+    new Achievement('Gained 10 likes on a post', 20, false),
+    new Achievement('Checked of actions in the checklist each day of a work week', 30, true),
+    new Achievement('Suggested a new checklist item', 30, false),
   ]
 
   constructor() { }
