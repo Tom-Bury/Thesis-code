bbe16d47a891512181fb174605ff94ccda4b440a
Tom Bury <tom.bury@hotmail.com>
2020-05-03 16:28:39 +0200

    Fix about page information on getting points for no forum access users

--- Firebase-project/src/app/about/about.component.ts
+++ Firebase-project/src/app/about/about.component.ts
@@ -4,6 +4,7 @@ import {
 import {
   AboutEntry
 } from '../shared/interfaces/about-entry.model';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-about',
@@ -32,9 +33,17 @@ export class AboutComponent {
     new AboutEntry('How is data gathered?',
       'database',
       '<span>The dashboard <b>does not gather any usage data.</b></span>\
-      <span>Data is only gathered via online <b>questionnaires</b> that the participants will fill in during the study.</span>'),
+      <span>Data is only gathered via online <b>questionnaires</b> that the participants will fill in during the study.</span>\
+      <span>The energy usage data seen in the dashboard is gathered by Wattness via sensors in the fuse box.</span>'),
 
-    new AboutEntry('How are points earned?',
+
+  ];
+
+  constructor(
+    private currUser: UserService
+  ) {
+    if (currUser.userHasForumAccess()) {
+      this.aboutEntries.push(new AboutEntry('How are points earned?',
       'trophy',
       '<span><i>Please note that right now the scoring system is <b>not implemented concretely</b>, since no in the wild study could be done.</span>\
       <span>So any scores you see now in the dashboard are static dummy values.</i></span>\
@@ -47,15 +56,31 @@ export class AboutComponent {
         <li>Being active on the discussion board: <b>posting, liking, commenting</b></li>\
         <li>Checking off sustainable actions you did in the <b>checklist</b></li>\
         <li><b>Exploring the data</b> via the charts in the explore tab</li>\
-        <il>Scoring one time only bonus points with <b>achievements</b></il>\
+        <li>Scoring one time only bonus points with <b>achievements</b></li>\
       </ul>\
       <span>The scores will <b>reset each weak</b> to give everyone a chance of becoming the winner for a week.</span>\
-      <span>An overview of this week\'s scores is given in the leaderboard.</span>'),
+      <span>An overview of this week\'s scores is given in the leaderboard.</span>')
+    );
+    } else {
+      this.aboutEntries.push(new AboutEntry('How are points earned?',
+      'trophy',
+      '<span><i>Please note that right now the scoring system is <b>not implemented concretely</b>, since no in the wild study could be done.</span>\
+      <span>So any scores you see now in the dashboard are static dummy values.</i></span>\
+      <span><i>It would have worked as follows:</i></span> \
+      <hr class="m-3">\
+      <span>Points can be earned in general by interacting with the dashboard.</span>\
+      <span>This means you will get points for:</span>\
+      <ul>\
+        <li><b>Daily usage</b> of the dahsboard</li>\
+        <li>Checking off sustainable actions you did in the <b>checklist</b></li>\
+        <li><b>Exploring the data</b> via the charts in the explore tab</li>\
+        <li>Scoring one time only bonus points with <b>achievements</b></li>\
+      </ul>\
+      <span>The scores will <b>reset each weak</b> to give everyone a chance of becoming the winner for a week.</span>\
+      <span>An overview of this week\'s scores is given in the leaderboard.</span>'),);
+    }
 
-
-  ];
-
-  constructor() {}
+  }
 
 
 
