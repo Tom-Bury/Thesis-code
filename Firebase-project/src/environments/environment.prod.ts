bf705896ee144f70a47e7ce5c8d52061c88d167a
Tom Bury <tom.bury@hotmail.com>
2020-05-29 18:03:57 +0200

    Add frontend environment files back

---
+++ Firebase-project/src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyB8CNGyGHEWc7EjcbZidkNnZA2irMryXP8',
+    authDomain: 'thesis-tom-bury.firebaseapp.com',
+    databaseURL: 'https://thesis-tom-bury.firebaseio.com',
+    projectId: 'thesis-tom-bury',
+    storageBucket: 'thesis-tom-bury.appspot.com',
+    messagingSenderId: '722001355031',
+    appId: '1:722001355031:web:8293d5d92425758ddd9752',
+    measurementId: 'G-MKQ5N0KV03'
+  },
+
+  apiBaseUrl: 'https://thesis-reverse-proxy.web.app/api',
+
+  needsAuthentication: true,
+
+  mainCollection: 'survey-03-05-a',
+
+  mainStorage: 'survey-03-05-a',
+
+  usersDB: 'users/',
+
+  forumDB: 'forum-posts/'
+};
